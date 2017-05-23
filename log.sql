5a72396bfde9188cc1a06c2edbbb89def666285d
Sagar Saini <Sagar Saini>
2017-05-23 16:31:38 +0530

    Final Commit

--- log.sql
+++ log.sql
@@ -11,8 +11,8 @@ create view toparticles as select articles.title,count(log.path) from log,articl
 --This view is used to fetch the top authors of the newspaper:
 create view topauthors as select authors.name,count(articles.author) from log,articles,authors where path like '/article%' and log.path = concat('/article/',articles.slug) and articles.author = authors.id group by articles.author,authors.name order by count(path) desc;
 
---this view is used to fetch the errors:
-create view alldayerrors as select date, count(status) from statistics where status != '200 OK' group by date union all select date,count(status) from statistics group by date;
-
 --This view includes the status and particular date from the timestamp column time
 create view statistics as select status,date(time) from log;
+
+--this view is used to fetch the errors:
+create view alldayerrors as select date, count(status) from statistics where status != '200 OK' group by date union all select date,count(status) from statistics group by date;
--- logproject.py
+++ logproject.py
@@ -12,50 +12,59 @@ def connect(database='news'):
 
 
 def articles_on_top():
-    db, c = connect()
-    q = "select * from toparticles"
-    c.execute(q)
-    ans = c.fetchall()
-    print '\nTop Articles:'
-    for x in xrange(0, 3):
-        print '%s -- %s views' % (ans[x][0], ans[x][1])
-    c.close()
-    db.close()
+    try:
+        db, c = connect()
+        q = "select * from toparticles"
+        c.execute(q)
+        ans = c.fetchall()
+        print '\nTop Articles:'
+        for x in xrange(0, 3):
+            print '%s -- %s views' % (ans[x][0], ans[x][1])
+        c.close()
+        db.close()
+    except:
+        print 'error in creating view toparticles!!'
 
 
 def authors_on_top():
-    db, c = connect()
-    q = "select * from topauthors"
-    c.execute(q)
-    ans = c.fetchall()
-    print '\nTop Authors:'
-    for x in xrange(0, len(ans)):
-        print '%s -- %s views' % (ans[x][0], ans[x][1])
-    db.commit()
-    c.close()
-    db.close()
+    try:
+        db, c = connect()
+        q = "select * from topauthors"
+        c.execute(q)
+        ans = c.fetchall()
+        print '\nTop Authors:'
+        for x in xrange(0, len(ans)):
+            print '%s -- %s views' % (ans[x][0], ans[x][1])
+        db.commit()
+        c.close()
+        db.close()
+    except:
+        print 'error in creating view topauthors!!!'
 
 
 def all_day_errors():
-    db, c = connect()
-    q = "select * from alldayerrors"
-    c.execute(q)
-    ans = c.fetchall()
-    length = len(ans)/2
-    date = []
-    error = []
-    for x in xrange(0, length):
-        # Calculating percentage
-        errorPercentage = float(ans[x][1] / float(ans[x + length][1])) * 100
-        if errorPercentage > 1:
-            date.append(ans[x][0])
-            error.append(float(errorPercentage))
-    print '\nDays on which more than 1% of requests lead to errors are:'
-    for x in xrange(0, len(date)):
-        print '%s -- %f' % (date[x], error[x])
-    db.commit()
-    c.close()
-    db.close()
+    try:
+        db, c = connect()
+        q = "select * from alldayerrors"
+        c.execute(q)
+        ans = c.fetchall()
+        length = len(ans)/2
+        date = []
+        error = []
+        for x in xrange(0, length):
+            # Calculating percentage
+            errorPercentage = float(ans[x][1] / float(ans[x + length][1])) * 100
+            if errorPercentage > 1:
+                date.append(ans[x][0])
+                error.append(float(errorPercentage))
+        print '\nDays on which more than 1% of requests lead to errors are:'
+        for x in xrange(0, len(date)):
+            print '%s -- %f' % (date[x], error[x])
+        db.commit()
+        c.close()
+        db.close()
+    except:
+        print 'error in creating view alldayerrors!!!!'
 
 
 if __name__ == '__main__':
